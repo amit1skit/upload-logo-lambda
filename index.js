0be5d84ab6448effffcaa8210c89b565e6b451cd
amit1skit <Amit1skit@gmail.com>
2020-12-23 16:31:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda! V2.0'),
+    };
+    return response;
+};
